b7a79b36429fd59eae2715f9f1334ae90cb727f1
Devin Jaggernauth <neexis@gmail.com>
2023-07-26 13:41:36 -0400

    add gradients to text

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import styles from './page.module.css';
+import { buttonVariants } from '@/components/ui/button';
 
 
 export default function Home() {
@@ -6,7 +8,16 @@ export default function Home() {
 
   return (
     <main className={styles.main}>
-      
+      <div className='py-6'>
+      <h1 className='font-bold text-4xl bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent text-center'>Patient Management, Simplified</h1>
+
+      </div>
+              <Link
+          href="/login"
+          className={buttonVariants({ variant: 'outline' })}
+        >
+          Go To Physician Login
+        </Link>
     </main>
   );
 }
