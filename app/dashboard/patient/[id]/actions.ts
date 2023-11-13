83f7eb807e155bc66a2fd591b7abb808a7b0a16d
Jeremiah <jeremiah.fallin@gmail.com>
2023-11-12 21:22:59 -0800

    allow user to edit patient exam

--- app/dashboard/patient/[id]/actions.ts
+++ app/dashboard/patient/[id]/actions.ts
@@ -1,12 +1,13 @@
-'use server';
-import {formSchema} from '@/components/EditPatientData';
-import { soapFormSchema } from '@/components/SoapAssessmentForm';
-import {createServerActionClient} from '@supabase/auth-helpers-nextjs';
-import {cookies} from 'next/headers';
-import {z} from 'zod';
+"use server";
+import { examFormSchema } from "@/components/NewPatientExam";
+import { formSchema } from "@/components/EditPatientData";
+import { soapFormSchema } from "@/components/SoapAssessmentForm";
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { z } from "zod";
 
 // Should this request be made as a client
-const supabase = createServerActionClient({cookies});
+const supabase = createServerActionClient({ cookies });
 
 export const editPatientData = async (
   patientData: z.infer<typeof formSchema>
@@ -28,8 +29,8 @@ export const editPatientData = async (
   } = patientData;
 
   try {
-    const {data, error} = await supabase
-      .from('Patients')
+    const { data, error } = await supabase
+      .from("Patients")
       .update({
         first_name: firstName,
         surname,
@@ -44,17 +45,18 @@ export const editPatientData = async (
         practice: practiceId,
         primary_provider: providerId,
       })
-      .eq('id', patientId);
+      .eq("id", patientId);
 
     if (error) throw new Error(`There was a problem: ${error}`);
   } catch (error: any) {
     if (error) {
-      throw new Error('Something went wrong: in actions file', error.message);
+      throw new Error("Something went wrong: in actions file", error.message);
     }
   }
 };
+
 export const updateSoapAssessment = async (
-  formData: z.infer<typeof soapFormSchema>,
+  formData: z.infer<typeof soapFormSchema>
 ) => {
   const supabase = createServerActionClient({ cookies });
   const {
@@ -73,9 +75,9 @@ export const updateSoapAssessment = async (
     patientId,
   } = formData;
 
-   try {
+  try {
     const { error } = await supabase
-      .from('soap_assessments')
+      .from("soap_assessments")
       .update({
         subjective_findings: subjectiveFindings,
         objective_findings: objectiveFindings,
@@ -89,13 +91,82 @@ export const updateSoapAssessment = async (
         urine,
         plan,
       })
-      .eq('patient', patientId)
-      .eq('id', id);
+      .eq("patient", patientId)
+      .eq("id", id);
 
     if (error) throw new Error(`There was a problem: ${error.message}`);
   } catch (error: unknown) {
     if (error) {
-      throw new Error('Something went wrong');
+      throw new Error("Something went wrong");
+    }
+  }
+};
+
+export const editClinicalRecord = async (
+  formData: z.infer<typeof examFormSchema>
+) => {
+  const supabase = createServerActionClient({ cookies });
+
+  const {
+    presentingComplaint,
+    historyPresentingComplaint,
+    pastMedicalHistory,
+    drugHistory,
+    familyHistory,
+    socialHistory,
+    allergies,
+    systemsReview,
+    onExamination,
+    observations,
+    focusedFindings,
+    bloodPressure,
+    heartRate,
+    respiratoryRate,
+    oxygenSaturation,
+    temperature,
+    randomBloodSugar,
+    urine,
+    differentialDiagnosis,
+    diagnosis,
+    plan,
+    patientId,
+    providerId,
+  } = formData;
+
+  try {
+    const { error } = await supabase
+      .from("clinical_records")
+      .update({
+        patient: patientId,
+        provider: providerId,
+        presenting_complaint: presentingComplaint,
+        history_presenting_complaint: historyPresentingComplaint,
+        past_medical_history: pastMedicalHistory,
+        drug_history: drugHistory,
+        family_history: familyHistory,
+        social_history: socialHistory,
+        allergies,
+        systems_review: systemsReview,
+        on_examination: onExamination,
+        observations,
+        focused_findings: focusedFindings,
+        blood_pressure: bloodPressure,
+        heart_rate: parseInt(heartRate as string),
+        respiratory_rate: parseInt(respiratoryRate as string),
+        oxygen_saturation: parseInt(oxygenSaturation as string),
+        temperature: parseInt(temperature as string),
+        random_blood_sugar: parseInt(randomBloodSugar as string),
+        urine,
+        differential_diagnosis: differentialDiagnosis,
+        diagnosis,
+        plan,
+      })
+      .eq("patient", patientId);
+
+    if (error) throw new Error(`There was a problem: ${error}`);
+  } catch (error: unknown) {
+    if (error) {
+      throw new Error("Something went wrong:", error);
     }
   }
 };
--- components/PatientClinicalExamCard.tsx
+++ components/PatientClinicalExamCard.tsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import {useEffect, useState} from 'react';
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-import {Button, buttonVariants} from './ui/button';
+import { Button, buttonVariants } from "./ui/button";
 import {
   Card,
   CardHeader,
   CardTitle,
   CardContent,
   CardDescription,
-} from './ui/card';
-import {ChevronRight} from 'lucide-react';
-import ExamDetails from './ExamDetails';
-import {format, parseISO} from 'date-fns';
-import {createClientComponentClient} from '@supabase/auth-helpers-nextjs';
-import {useRouter} from 'next/navigation';
+} from "./ui/card";
+import { ChevronRight } from "lucide-react";
+import ExamDetails from "./ExamDetails";
+import { format, parseISO } from "date-fns";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 export default function PatientClinicalExamCard({
   clinicalAssessment,
@@ -31,28 +31,28 @@ export default function PatientClinicalExamCard({
   const clinicalAssessmentExists =
     clinicalAssessment !== undefined && clinicalAssessment.length > 0;
 
-  const {id} = patientData;
+  const { id } = patientData;
 
-  // useEffect(() => {
-  //   const channel = supabase
-  //     .channel('clinical exams')
-  //     .on(
-  //       'postgres_changes',
-  //       {
-  //         event: '*',
-  //         schema: 'public',
-  //         table: 'clinical_records',
-  //       },
-  //       () => {
-  //         router.refresh();
-  //       }
-  //     )
-  //     .subscribe();
+  useEffect(() => {
+    const channel = supabase
+      .channel("clinical exams")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "clinical_records",
+        },
+        () => {
+          router.refresh();
+        }
+      )
+      .subscribe();
 
-  //   return () => {
-  //     supabase.removeChannel(channel);
-  //   };
-  // }, [supabase, router]);
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase, router]);
 
   return (
     <>
@@ -67,7 +67,7 @@ export default function PatientClinicalExamCard({
             </CardDescription>
             <Link
               href={`/dashboard/new/exam/${id}`}
-              className={buttonVariants({variant: 'default'})}
+              className={buttonVariants({ variant: "default" })}
             >
               Clerk Now <ChevronRight className="h-4 w-4" />
             </Link>
@@ -76,8 +76,8 @@ export default function PatientClinicalExamCard({
           <CardContent className="flex flex-col items-center">
             {clinicalAssessmentExists ? (
               <CardDescription className="text-sm mb-4">
-                Exam Date:{' '}
-                {format(parseISO(clinicalAssessment[0].exam_date!), 'PPP')}
+                Exam Date:{" "}
+                {format(parseISO(clinicalAssessment[0].exam_date!), "PPP")}
               </CardDescription>
             ) : null}
             <Button onClick={() => setIsExamDetailsOpen(true)}>
