d29f8404231afdf455ae7c4b1de385d55220e1b7
Devin Jaggernauth <neexis@gmail.com>
2023-11-15 22:48:15 -0400

    update landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,17 +16,22 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <header className='py-3 px-4 w-full'>
+          <Link href='/dashboard'>
+
           <h2 className='font-semibold text-lg bg-gradient-to-br from-cyan-500 to-blue-900 bg-clip-text text-transparent'>Clerkwise</h2>
+          </Link>
         </header>
         <main className='max-w-3xl w-full sm:px-4 px-2 mx-auto'>
 
         {children}
         </main>
-        <footer className='w-full p-4 pt-16 '>
+        <footer className='w-full p-4 pt-16'>
           <p className='text-center'>&copy; {new Date().getFullYear()} Full Stack Collective </p>
         </footer>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,20 @@ import Link from 'next/link';
 import styles from './page.module.css';
 import { buttonVariants } from '@/components/ui/button';
 
-
 export default function Home() {
-
-
   return (
-    <main className={styles.main}>
-      <div className='py-6'>
-      <h1 className='font-bold text-4xl bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent text-center'>Patient Management, Simplified</h1>
-
-      </div>
-              <Link
-          href="/login"
-          className={buttonVariants({ variant: 'outline' })}
-        >
+    <main className="min-h-[70vh] flex items-center">
+      <div className="bg-[url('../public/main-bg.jpg')] bg-no-repeat absolute w-full h-full bg-cover z-[-2] top-0 left-0 opacity-10"></div>
+      <div className="flex flex-col items-center p-24 gap-3 bg-white bg-opacity-70 rounded-3xl">
+        <div className="py-6">
+          <h1 className="font-bold text-4xl bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent text-center">
+            Patient Management, Simplified
+          </h1>
+        </div>
+        <Link href="/login" className={buttonVariants({ variant: 'outline' })}>
           Go To Physician Login
         </Link>
+      </div>
     </main>
   );
 }
